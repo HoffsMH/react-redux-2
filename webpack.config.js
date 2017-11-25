bc721f84ae4e96b79975ae45b265062a26c8a1ce
HoffsMH <hecker.matthew@gmail.com>
2017-11-24 22:15:26 -0500

    webpack scaffolding

--- webpack.config.js
+++ webpack.config.js
@@ -2,13 +2,29 @@ const path = require('path');
 var HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-  entry: './src/index.js',
+  context: path.join(__dirname, 'src'),
+  entry: './index.js',
   output: {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'dist')
   },
-
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: [
+          'babel-loader',
+        ],
+      },
+    ],
+  },
   plugins: [new HtmlWebpackPlugin({
-    template: './src/index.html'
-  })]
+    template: './index.html'
+  })],
+  resolve: {
+    modules: [
+      path.join(__dirname, 'node_modules'),
+    ],
+  }
 };
